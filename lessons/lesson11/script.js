7936dfc8c0f5c2edfae44681183b6bdf6e1f209a
Your Name <ayalab2502@gmail.com>
2025-12-24 19:00:53 +0200

    project

---
+++ lessons/lesson11/script.js
@@ -0,0 +1,22 @@
+function welcome() {
+    alert("ברוכים הבאים לאתר שלי!");
+}
+
+function welcome2() {
+    let un = document.getElementById("username").value;
+
+    alert("ברוך הבא " + un + "!!!");
+}
+
+function multi50() {
+    let mul = document.getElementById("num1").value;
+    alert(mul * 50);
+}
+
+function multi12() {
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    alert(num1 * num2);
+
+}
