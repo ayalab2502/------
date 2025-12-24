a6d5e94001ba1f25f42864f07060904318322012
Your Name <ayalab2502@gmail.com>
2025-12-24 21:01:24 +0200

    project

--- lessons/lesson12/script.js
+++ lessons/lesson12/script.js
@@ -9,3 +9,24 @@ function task2() {
     document.getElementById("counter2").innerHTML = ++counter2;
     document.getElementById("counter2").style.fontSize = (12 + counter2) + 'px';
 }
+
+function task3() {
+    const answer = document.getElementById("answer").value;
+    if (answer == "צהוב") {
+        document.getElementById("output").innerHTML = "נכון";
+    } else {
+        document.getElementById("output").innerHTML = "לא נכון";
+    }
+}
+
+
+
+function task4() {
+    const answer = document.getElementById("city").value; 
+
+    if (answer == "ירושלים") {
+        document.getElementById("output2").innerHTML = "נכון";
+    } else {
+        document.getElementById("output2").innerHTML = "לא נכון";
+    }
+}
